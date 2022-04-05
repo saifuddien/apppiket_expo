40fc8120b64c801a4a5634258460e9b51cd99271
saifuddien <ariefsaifuddien01@gmail.com>
2022-04-05 23:28:45 +0200

    style

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-import { StyleSheet } from 'react-native';
 import WebView from 'react-native-webview';
 import StatusBarCustom from 'react-native-custom-statusbar';
 
 export default function App() {
   return (
     <StatusBarCustom backgroundColor="#f59800" barStyle="light-content">
-      <WebView style={styles.container} source={{ uri: 'https://piketsabtu.netlify.app' }} />
+      <WebView source={{ uri: 'https://piketsabtu.netlify.app' }} />
     </StatusBarCustom>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 35,
-    borderTopWidth: 2,
-    borderTopColor: 'black'
-  },
-});
