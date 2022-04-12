078f50b59c00b7f7bad5ac5c62b604debeb7cf73
saifuddien <ariefsaifuddien01@gmail.com>
2022-04-13 01:45:59 +0200

    okeo

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import StatusBarCustom from 'react-native-custom-statusbar';
 export default function App() {
   return (
     <StatusBarCustom backgroundColor="#f59800" barStyle="light-content">
-      <WebView source={{ uri: 'https://piketsabtu.netlify.app' }} />
+      {/* <WebView source={{ uri: 'https://piketsabtu.netlify.app' }} /> */}
+      <WebView source={{ uri: 'https://piketvue.vercel.app' }} />
     </StatusBarCustom>
   );
 }
